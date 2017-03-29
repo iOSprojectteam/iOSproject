db0e349101286eec3eef7b4acbf3ae8b358a4577
Xcode User <xcode@fasts-macbook-pro.local>
2017-03-29 18:34:38 -0400

    TemplateCollectionViewController

---
+++ iOS_project_ryan/TemplateCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TemplateCollectionViewController.h
+//  iOS_project_ryan
+//
+//  Created by Xcode User on 2017-03-29.
+//  Copyright © 2017 Ryan Falcon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TemplateCollectionViewController : UICollectionViewController
+
+@end
