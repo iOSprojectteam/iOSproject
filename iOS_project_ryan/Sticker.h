d2b03fc4de429eae6b6da1ab86917a9dc0bfbc37
Ryan West <ryanwest_ca@yahoo.com>
2017-04-11 19:10:16 -0400

    sticker class

--- iOS_project_ryan/Sticker.h
+++ iOS_project_ryan/Sticker.h
@@ -9,5 +9,19 @@
 #import <Foundation/Foundation.h>
 
 @interface Sticker : NSObject
+{
+    NSInteger *x;
+    NSInteger *y;
+    NSInteger *width;
+    NSInteger *height;
+    NSInteger *rotation;
+    NSString *fileName;
+}
 
+@property (nonatomic) NSInteger *x;
+@property (nonatomic) NSInteger *y;
+@property (nonatomic) NSInteger *width;
+@property (nonatomic) NSInteger *height;
+@property (nonatomic) NSInteger *rotation;
+@property (nonatomic, strong) NSString *fileName;
 @end
