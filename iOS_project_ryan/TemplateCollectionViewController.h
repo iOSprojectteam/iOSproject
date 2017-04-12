c1e83d3aa13bd8e7602833c1555f62f23ced8cb8
Xcode User <xcode@fasts-macbook-pro.local>
2017-04-12 19:38:15 -0400

    Updated

--- iOS_project_ryan/TemplateCollectionViewController.h
+++ iOS_project_ryan/TemplateCollectionViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TemplateCollectionViewController : UICollectionViewController <UITextFieldDelegate> {
+@interface TemplateCollectionViewController : UIViewController <UITextFieldDelegate> {
     
 }
 
