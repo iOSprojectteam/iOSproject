951d7aaa036744d67b945d8d800ac9d5374fbf4b
Xcode User <xcode@Fasts-MacBook-Pro.local>
2017-04-17 09:19:41 -0400

    pushed

--- iOS_project_ryan/ViewController.h
+++ iOS_project_ryan/ViewController.h
@@ -3,7 +3,7 @@
 //  iOS_project_ryan
 //
 //  Created by Ryan Galimova on 2017-03-12.
-//  Copyright © 2017 Ryan Galimova. All rights reserved.
+//  Copyright © 2017 Ryan Galimova. All rights reserved..
 //
 
 #import <UIKit/UIKit.h>
